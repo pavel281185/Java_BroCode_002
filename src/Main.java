a189b25c2e8b3ee9a43442f9d6fc4221583bda2c
pavel281185 <pavelkvasnicka@gmail.com>
2023-04-13 12:59:19 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        int x; //declaration
+        x = 123; //assigment
+        System.out.println(x);
+        System.out.println("x");
+
+        int y = 123; //initialization
+        System.out.println("My number is: " + x);
+
+        long a = 23737234523482374L;
+        System.out.println(a);
+
+        byte b = 100;
+        System.out.println(b);
+
+        double w = 123.01;
+        System.out.println(w);
+
+        float p = 3.14f;
+        System.out.println(p);
+
+        double o = 3.14;
+        System.out.println(o);
+
+        boolean t = false;
+        System.out.println(t);
+
+        char symbol = '@';
+        System.out.println(symbol);
+
+        String name = "Pavel";
+        System.out.println("Hello " + name);
+    }
+}
